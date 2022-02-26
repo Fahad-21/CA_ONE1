c45292e3d4f47da44594a06287d8e7238ad77c4f
Fahad Ali <69964008+Fahad-21@users.noreply.github.com>
2022-02-26 18:10:49 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import unittest
+
+updated_dictionary_list = []
+employee_dict = {'StaffID': '', 'LastName': "", "FirstName": '','RegHours':'', 'HourlyRate': '', 'OTMultiple': "", 'TaxCredit': '', 'StandardBand': ''}
+hours_dict = {'StaffID': '', 'Date': '','HoursWorked': ''}

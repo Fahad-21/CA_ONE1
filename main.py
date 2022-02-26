a4ea3f2e062a93ea29463b036490838cdc0e270a
Fahad Ali <69964008+Fahad-21@users.noreply.github.com>
2022-02-26 18:12:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,15 @@ def computeAllPayment(employeeFile,hoursFile):
         employee_dict.update({'StandardBand': new_row[7]})
         updated_dictionary_list.append(employee_dict)
     hour_data = open(hoursFile, 'r')
+for row in hour_data:
+        new_hour_row = row.replace('\n', '')
+        new_row_2 = new_hour_row.split(' ')
+        hours_dict.update({'StaffID': new_row_2[0]})
+        hours_dict.update({'Date': new_row_2[1]})
+        hours_dict.update({'HoursWorked': new_row_2[2]})
+        updated_dictionary_list.append(hours_dict)
+        employee = Employee()
+        employee.computePayment(new_row_2[2], new_row_2[1])
+    employee_data.close()
+    hour_data.close()
+    return updated_dictionary_list
